d7b44bdd92121d56e1b33b1fa1f668dfd8907fee
Lienkulet <clocicovalexandru@gmail.com>
2026-02-17 16:07:10 +0200

    contact card footer left block

--- src/features/ContactCardFooter/CardFooterLeft.jsx
+++ src/features/ContactCardFooter/CardFooterLeft.jsx
@@ -1,13 +1,50 @@
-import React from 'react'
+import ClockIcon from '@/components/Icons/ClockIcon'
+import './CardFooterLeft.css'
+import HourCard from './HourCard'
+import PhoneEmailIcon from '@/components/Icons/PhoneEmailIcon'
+import WaypointIcon from '@/components/Icons/WaypointIcon'
+import MapIcon from '@/components/Icons/MapIcon'
+import Link from 'next/link'
 
 const CardFooterLeft = () => {
   return (
     <div className='left-block'>
-          <h1>Get Quick Free <br />
-            Professional Consultation</h1>
+      <h1>Get Quick Free <br />
+        Professional Consultation</h1>
+      <HourCard
+        hours={[
+          { days: "Mon–Fri :", open: "9:00 am - 10:00 pm" },
+          { days: "Sat–Sun :", open: "9:00 am - 8:00 pm" },
+        ]}
+        icon={<ClockIcon />}
+      />
 
-            <div className='line'></div>
-            <div className='line'></div>
+      <div className='line'></div>
+      <HourCard
+        hours={[
+          { days: "Phone :", open: "+(690) 2560 0020" },
+          { days: "Email :", open: "booking@healthpoint.com" },
+        ]}
+        icon={<PhoneEmailIcon />}
+      />
+
+      <div className='line'></div>
+      <HourCard
+        hours={[
+          { days: "Address :", open: "Medprestige" },
+          { days: "", open: "4263 Wilkinson Street" },
+          { days: "", open: "Tennessee" },
+        ]}
+        icon={<WaypointIcon />}
+      />
+
+      <Link
+        href={'/contact#mapID'}
+        className='locationBtn'
+      >
+        <MapIcon />
+        <span>View Location Map</span>
+      </Link>
     </div>
   )
 }
