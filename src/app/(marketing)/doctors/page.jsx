89db2532eba29482a9e0999972de12172ed165eb
Vladosc22 <chiochiu.vlad@gmail.com>
2026-02-17 14:10:08 +0200

    added Doctor Details Page

--- src/app/(marketing)/doctors/page.jsx
+++ src/app/(marketing)/doctors/page.jsx
@@ -1,20 +1,101 @@
-import React from 'react'
-import PagesHero from '../../../components/PagesHero/PagesHero'
-import Link from "next/link";
+import React from "react";
+import PagesHero from "@/components/PagesHero/PagesHero";
+import DoctorCard from "@/components/DoctorCard";
 
-const Doctors = () => {
-  return(
-   <div className="doctors">
-    <PagesHero title={'Meet Our Doctors'} subtitle={'Letraset sheets containing Lorem Ipsum passages and more recently with desktop publishing'} />
-    <a href={"/doctors/1"} className="doctor-card">
-      <img src="https://images.unsplash.com/photo-1505751172876-fa1923c5c528?w=400&h=400&fit=crop&crop=center" alt="Doctor 1" className="doctor-image" />
-      <div className="doctor-info">
-        <h3 className="doctor-name">Dr. John Doe</h3>
-        <p className="doctor-specialty">Cardiologist</p>
-      </div>
-    </a>
-   </div>
-  )
-}
+const doctors = [
+    {
+        id: 1,
+        name: "Dr. Jessica Joan",
+        specialty: "Nephrology",
+        image: "/doctor1.jpg",
+        hasSocial: false,
+    },
+    {
+        id: 2,
+        name: "Dr. Alexandra",
+        specialty: "Gastroenterology",
+        image: "/doctor1.jpg",
+        hasSocial: true,
+    },
+    {
+        id: 3,
+        name: "Dr. Kimberly Hayes",
+        specialty: "Neurology",
+        image: "/doctor1.jpg",
+        hasSocial: false,
+    },
+    {
+        id: 4,
+        name: "Dr. Bella Carol",
+        specialty: "Obstetrics",
+        image: "/doctor1.jpg",
+        hasSocial: true,
+    },
+    {
+        id: 5,
+        name: "Dr. Rebecca Rose",
+        specialty: "Gynecology",
+        image: "/doctor1.jpg",
+        hasSocial: false,
+    },
+    {
+        id: 6,
+        name: "Dr. Stephanie Sue",
+        specialty: "Haematology",
+        image: "/doctor1.jpg",
+        hasSocial: true,
+    },
+    {
+        id: 7,
+        name: "Dr. Penelope Morgan",
+        specialty: "Physiotherapy",
+        image: "/doctor1.jpg",
+        hasSocial: false,
+    },
+    {
+        id: 8,
+        name: "Dr. Lauren Leah",
+        specialty: "Oncology",
+        image: "/doctor1.jpg",
+        hasSocial: true,
+    },
+];
 
-export default Doctors
+
+const MeetOurDoctors = () => {
+    return (
+        <main>
+            <PagesHero
+                title="Meet Our Doctors"
+                subtitle="Letraset sheets containing Lorem Ipsum passages and more recently with desktop publishing"
+            />
+
+            <section
+                style={{
+                    padding: "116px 0",
+                    maxWidth: "1200px",
+                    margin: "0 auto",
+                }}
+            >
+                <div
+                    style={{
+                        display: "flex",
+                        flexWrap: "wrap",
+                        gap: "30px",
+                        justifyContent: "center",
+                    }}
+                >
+                    {doctors.map((doctor, index) => (
+                        <DoctorCard
+                            key={doctor.id}
+                            {...doctor}
+                            index={index}
+                        />
+                    ))}
+                </div>
+            </section>
+        </main>
+    );
+};
+
+export default MeetOurDoctors;
