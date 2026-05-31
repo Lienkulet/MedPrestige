5437aa70bcd1906d690abdae546438b4f1e86b30
Vladosc22 <chiochiu.vlad@gmail.com>
2026-05-31 17:35:16 +0300

    created Doctor Repo inside Data Acc LAy/Repo

---
+++ backend/MedPrestige.DataAccessLayer/Repositories/DoctorRepository.cs
@@ -0,0 +1,73 @@
+using MedPrestige.DataAccessLayer.Context;
+using MedPrestige.DataAccessLayer.Interfaces;
+using MedPrestige.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace MedPrestige.DataAccessLayer.Repositories
+{
+    public class DoctorRepository : IDoctorRepository
+    {
+        private readonly AppDbContext _context;
+
+        public DoctorRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public List<Doctor> GetAll()
+        {
+            return _context.Doctors
+                .Include(d => d.User)
+                .Include(d => d.DoctorDetails)
+                .Include(d => d.DoctorServices)
+                .ToList();
+        }
+
+        public Doctor GetById(int id)
+        {
+            return _context.Doctors
+                .Include(d => d.User)
+                .Include(d => d.DoctorDetails)
+                .Include(d => d.DoctorServices)
+                .FirstOrDefault(d => d.DoctorId == id);
+        }
+
+        public Doctor GetByUserId(int userId)
+        {
+            return _context.Doctors
+                .Include(d => d.User)
+                .Include(d => d.DoctorDetails)
+                .FirstOrDefault(d => d.UserId == userId);
+        }
+
+        public List<Doctor> GetByStatus(string status)
+        {
+            return _context.Doctors
+                .Include(d => d.User)
+                .Where(d => d.Status == status)
+                .ToList();
+        }
+
+        public void Add(Doctor doctor)
+        {
+            _context.Doctors.Add(doctor);
+            _context.SaveChanges();
+        }
+
+        public void Update(Doctor doctor)
+        {
+            _context.Doctors.Update(doctor);
+            _context.SaveChanges();
+        }
+
+        public void Delete(int id)
+        {
+            var doctor = _context.Doctors.Find(id);
+            if (doctor != null)
+            {
+                _context.Doctors.Remove(doctor);
+                _context.SaveChanges();
+            }
+        }
+    }
+}
